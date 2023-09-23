49057d6fa5e054bce204a6704e7976f27d71e32d
Klaire Curde <kcurde@gmail.com>
2023-09-23 15:25:24 -0400

    the start of a working CreateGuestbook endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use tonic::transport::Server;
 use tonic::{Request, Response, Status};
 
 #[derive(Debug)]
@@ -9,35 +10,81 @@ pub mod guestbook {
 
 // why?? crate::guestbook::entries_server::Entries
 use guestbook::guestbooks_server::{Guestbooks, GuestbooksServer};
-use guestbook::{Guestbook, GetGuestbookRequest, GetGuestbooksRequest, GuestbookResponse, CreateGuestbook, CreateGuestbookResponse, GuestbooksResponse, SearchGuestbookRequest, UpdateGuestbookRequest, UpdateGuestbookResponse, DeleteGuestbookRequest, DeleteGuestbookResponse};
+use guestbook::{
+    CreateGuestbook, CreateGuestbookResponse, DeleteGuestbookRequest, DeleteGuestbookResponse,
+    GetGuestbookRequest, GetGuestbooksRequest, Guestbook, GuestbookResponse, GuestbooksResponse,
+    SearchGuestbookRequest, UpdateGuestbookRequest, UpdateGuestbookResponse,
+};
 
 #[tonic::async_trait]
 impl Guestbooks for GuestbookService {
-    async fn create(&self, req: Request<CreateGuestbook>) -> Result<Response<CreateGuestbookResponse>, Status> {
+    async fn create(
+        &self,
+        req: Request<CreateGuestbook>,
+    ) -> Result<Response<CreateGuestbookResponse>, Status> {
+        println!("Got a request {:?}.", req);
+        let CreateGuestbook {
+            start_date, end_date, host, image_path, entries
+        } = req.into_inner();
+
+        let reply = guestbook::Guestbook {
+            id: 69,
+            start_date: 1,
+            end_date: 2,
+            host: "Potter".to_owned(),
+            image_path: "".to_owned(),
+            entries: Vec::new(),
+        };
+
+        Ok(Response::new(CreateGuestbookResponse { guestbook: Some(reply) }))
+    }
+
+    async fn get(
+        &self,
+        req: Request<GetGuestbookRequest>,
+    ) -> Result<Response<GuestbookResponse>, Status> {
         unimplemented!()
     }
 
-    async fn get(&self, req: Request<GetGuestbookRequest>) -> Result<Response<GuestbookResponse>, Status> {
+    async fn get_many(
+        &self,
+        req: Request<GetGuestbooksRequest>,
+    ) -> Result<Response<GuestbooksResponse>, Status> {
         unimplemented!()
     }
 
-    async fn get_many(&self, req: Request<GetGuestbooksRequest>) -> Result<Response<GuestbooksResponse>, Status> {
+    async fn search(
+        &self,
+        req: Request<SearchGuestbookRequest>,
+    ) -> Result<Response<GuestbooksResponse>, Status> {
         unimplemented!()
     }
 
-    async fn search(&self, req: Request<SearchGuestbookRequest>) -> Result<Response<GuestbooksResponse>, Status> {
+    async fn update(
+        &self,
+        req: Request<UpdateGuestbookRequest>,
+    ) -> Result<Response<UpdateGuestbookResponse>, Status> {
         unimplemented!()
     }
 
-    async fn update(&self, req: Request<UpdateGuestbookRequest>) -> Result<Response<UpdateGuestbookResponse>, Status> {
-        unimplemented!()
-    }
-
-    async fn delete(&self, req: Request<DeleteGuestbookRequest>) -> Result<Response<DeleteGuestbookResponse>, Status> {
+    async fn delete(
+        &self,
+        req: Request<DeleteGuestbookRequest>,
+    ) -> Result<Response<DeleteGuestbookResponse>, Status> {
         unimplemented!()
     }
 }
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:10000".parse().unwrap();
+
+    let guest_book = GuestbookService {};
+
+    let svc = GuestbooksServer::new(guest_book);
+
+    println!("Server running on {}... in theory...", addr);
+    Server::builder().add_service(svc).serve(addr).await?;
+
+    Ok(())
 }
